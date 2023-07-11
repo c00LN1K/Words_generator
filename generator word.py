43105650cf3c20ac262351353aa5559102fbfd1c
c00LN1K <138148415+c00LN1K@users.noreply.github.com>
2023-07-11 17:21:38 +0300

    Second step
    create bigramms

--- generator word.py
+++ generator word.py
@@ -2,12 +2,18 @@ import nltk
 
 from nltk.probability import FreqDist
 
-file = input('Введите название файла: corpus.txt or other\n> ')
+while True:
+    try:
+        file = input('Введите название файла: corpus.txt\n> ')
+        file = 'corpus.txt'
+        file = open(file, encoding='utf-8')
+        print("Файл успешно открыт")
+        break
+    except Exception as ex:
+        print('Неудачная попытка открыть файл. Попробуйте ещё раз\n')
 
-file = open(file, encoding='utf-8')
 text = file.read()
 file.close()
-
 
 tokens = nltk.regexp_tokenize(text, "[^ \t\n]+")
 
@@ -27,6 +33,26 @@ while (k != 'exit'):
         print("TypeError")
     except IndexError as ex:
         print("IndexError. Пожалуйста, введите целое число, которое находится в диапазоне корпуса.")
+    except ValueError as ex:
+        print("ValueError. Пожалуйста, введите число.")
+    finally:
+        k = input('> ')
+
+bigrams = nltk.bigrams(tokens)
+bigrams = list(bigrams)
+print(f'Количество биграмм: {len(bigrams)}')
+
+k = input(
+    f'Введите номер интересующей биграммы в диапазоне от {-len(bigrams)} до {len(bigrams) - 1} или exit для выхода\n> ')
+while (k != 'exit'):
+    try:
+        k = int(k)
+        head, tail = bigrams[k]
+        print(f'Голова: {head:10}\tХвост: {tail} ')
+    except TypeError as ex:
+        print("TypeError")
+    except IndexError as ex:
+        print("IndexError. Пожалуйста, введите целое число, которое находится в предложенном диапазоне.")
     except ValueError as ex:
         print("ValueError. Пожалуйста, введите число.")
     finally:
